3f8498ed81f81966f7c5eb1da8ed23ed5a2bc8f7
Wired Turtle <WiredTurtle@users.noreply.github.com>
2017-07-31 21:06:58 -0700

    Update pi_node.py

--- pi_node.py
+++ pi_node.py
@@ -4,11 +4,12 @@ import hashlib
 import urllib2
 
 location = '1'
-#Blacklisted access points to diminish database bloat.
+# Blacklisted access points to diminish database bloat.
 blacklist = ['64:d8:14','64:d9:89','64:ae:0c','10:bd:18','58:94:6b','00:00:00','00:c0:ca','b8:27:eb','00:0d:67','a0:cf:5b','cc:fb:65','b8:ae:6e','7c:bb:8a','80:19:34','ba:ae:6e','14:dd:a9','92:c7:92']
 
 
 try:
+	# Collection of mac-address probe requests covered in Vivek's securitytube video.
         def PacketHandler(packet):
                 if packet.haslayer(Dot11):
                         dot11_layer = packet.getlayer(Dot11)
